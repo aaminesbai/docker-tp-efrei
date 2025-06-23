0a954c6dfd5732b569c3cbf2c26248319a2686a5
it4 <it4@ntymail.com>
2025-06-23 11:33:37 +0200

    feat: add sample go app

---
+++ tp/1/sample_app/go.mod
@@ -0,0 +1,5 @@
+module catserver
+
+go 1.21
+
+require github.com/fatih/color v1.15.0
--- tp/1/sample_app/main.go
+++ tp/1/sample_app/main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"net/http"
 	"time"
+	"github.com/fatih/color"
 )
 
 var cats = []string{
@@ -34,6 +35,7 @@ func getRandomCatHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/cat", getRandomCatHandler)
 	log.Println("I'm listening on TCP port 8080")
+	color.Green("Server active !")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
