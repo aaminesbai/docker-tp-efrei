0c1541397bd92f00833968590647b829cda74063
it4 <it4@ntymail.com>
2025-06-23 11:29:15 +0200

    feat: add sample go app

---
+++ tp/1/sample_app/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+var cats = []string{
+	"meow",
+	`|\---/|
+| o_o |
+ \_^_/`,
+ ` /\_/\
+( o.o )
+ > ^ <`,
+}
+
+type CatResponse struct {
+	Art string `json:"art"`
+}
+
+func getRandomCatHandler(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().UnixNano())
+	cat := cats[rand.Intn(len(cats))]
+
+	resp := CatResponse{Art: cat}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
+
+func main() {
+	http.HandleFunc("/cat", getRandomCatHandler)
+	log.Println("I'm listening on TCP port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
